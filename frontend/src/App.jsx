0abc35f7101f8d2c76f4b6c5de4c5febe686d3fe
Made Ardhya Bayu Merada <117011844+madeubaymerada123@users.noreply.github.com>
2025-09-25 10:50:28 +0700

    theme no done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,6 +52,8 @@ import Payslips from "./pages/payroll/Payslips";
 import Sidebar from "./components/layout/Sidebar";
 import Topbar from "./components/layout/Topbar";
 
+//Theme Provider
+import { ThemeProvider } from "./context/ThemeContext";
 
 import { isTokenExpired, getToken, clearToken } from "./utils/auth";
 import { UserProvider } from "./context/UserContext";
@@ -288,7 +290,9 @@ function App() {
 
     return (
     <UserProvider>
-      <LayoutWrapper />
+      <ThemeProvider>     {/* ✅ wrap theme provider here */}
+        <LayoutWrapper />
+      </ThemeProvider>
     </UserProvider>
   );
 }
